55dc7a0fddd37a4aa5399fcc33d7ca5e9e4a4810
David Poza Suárez <pozasuarez@gmail.com>
2021-10-14 18:48:28 +0200

    board creation

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { PHRASAL_VERBS } from 'consts';
-import { transposeObject } from 'utils';
+import { transposeObject, shuffle } from 'utils';
+
+const words = {
+  ...PHRASAL_VERBS,
+  ...transposeObject(PHRASAL_VERBS)
+};
 
 export default function Board() {
-  const words = {
-    ...PHRASAL_VERBS,
-    ...transposeObject(PHRASAL_VERBS)
-  };
-  console.log(words);
-  function fillBoard(unsortedList = [], rows, cols) {
+  const [board, setBoard] = useState();
 
-  }
+  const createBoard = useCallback((rows, cols) => {
+    let board = [];
+    const wordList = [];
+    Object.keys(words).forEach((w) => {
+      wordList.push(w);
+    });
+    shuffle(wordList);
+    for (let i = 0; i < rows; i++ ) {
+      board[i] = [];
+      for (let j = 0; j < cols; j++ ) {
+        console.log(i,j)
+        board[i][j] = {
+          word: wordList[i*cols + j],
+          hidden: true,
+        };
+      }
+    }
+    return board;
+  }, []);
+
+  useEffect(() => {
+    setBoard(createBoard(2, 2));
+  }, [createBoard]);
+
   return (
-    <div></div>
+    <div>
+
+    </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,11 @@ export function transposeObject(obj) {
   });
   return transposed;
 }
+
+export function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
